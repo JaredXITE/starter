03d604fa415387e5e866b0393677ba781691a85c
Jared Chu <jared@xite.com>
2022-05-16 16:37:11 +0200

    codingchallenges

---
+++ codingchallenge2.js
@@ -0,0 +1,17 @@
+/* const markMass = 78;
+const heightMark = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / heightMark ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const whohigherBMI = markBMI > johnBMI;
+console.log(markBMI);
+console.log(johnBMI);
+console.log(whohigherBMI); */
+
+if (markBMI > johnBMI) {
+    console.log("Mark's bmi ${markBMI} a higher bmi!")
+} else {
+    console.log("John has a higher bmi!")
+};
