d0be89422b0faa36e76201f9a22de66883fa9aa5
Jared Chu <jared@xite.com>
2022-05-17 13:38:33 +0200

    added a function example, other ones coming

--- functions.js
+++ functions.js
@@ -2,4 +2,24 @@
 
 //expression examples
 
-//arrow function examples
+
+
+
+
+
+
+
+// function examples
+
+
+
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population}  million people and it's capital city is ${capitalCity}`;
+}
+
+const descNL = describeCountry('The Netherlands', 17, 'Amsterdam');
+const descGermany = describeCountry('Germany', 83, 'Berlin');
+const descFinland = describeCountry('Finland', 6, 'Helsinki');
+
+console.log(descFinland, descGermany, descNL);
