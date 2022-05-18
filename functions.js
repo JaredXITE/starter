f41dc9be1602bc9aca7f9449d1b4082247a62e99
Jared Chu <jared@xite.com>
2022-05-18 10:26:40 +0200

    added functions

--- functions.js
+++ functions.js
@@ -1,7 +1,29 @@
 //declaration examples
 
-//expression examples
+function smartAge1(birthYear) {
+    return 2022 - birthYear;
+}
+const age1 = smartAge1(2003);
 
+console.log(age1);
+
+//expression examples
+const smartAge2 = function (birthYear) {
+    return 2022 - birthYear;
+}
+
+const age2 = smartAge2(2003);
+console.log(age2); 
+
+
+
+
+
+//arrow function example
+
+const smartAge3 = (birthYear) => 2022 - birthYear 
+    const age3 = smartAge3(2003)
+console.log(age3);
 
 
 
