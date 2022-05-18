22bf2b2a4725f15765478013ca10aae89674808a
Jared Chu <jared@xite.com>
2022-05-18 15:03:41 +0200

    added a calling function example

--- functions.js
+++ functions.js
@@ -16,7 +16,40 @@ console.log(age2);
 
 //arrow function example
 const smartAge3 = (birthYear) => 2022 - birthYear 
+const smartAge4 = (birthYear) => {
+  return 2022 - birthYear;
+}
 
 const age3 = smartAge3(2003)
 console.log(age3);
+
+// functions example
+
+/*function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+  return juice;
+}
+
+let fruits = fruitProcessor(5, 0)
+console.log(fruits);
+fruits = fruitProcessor(9, 0)
+console.log(fruits); */
+
+
+//calling functions example
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples)
+  const orangePieces = cutFruitPieces(oranges)
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`
+  console.log(juice);
+  return juice;
+}
+console.log(fruitProcessor (4, 7))
+
+
 
