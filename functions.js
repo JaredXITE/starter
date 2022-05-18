ce6e3b712b2d3df0daa2265c1092025815832c3d
Jared Chu <jared@xite.com>
2022-05-18 15:51:41 +0200

    added coding challenge

--- functions.js
+++ functions.js
@@ -43,13 +43,29 @@ function cutFruitPieces(fruit) {
 }
 
 function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples)
-  const orangePieces = cutFruitPieces(oranges)
-  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`
-  console.log(juice);
-  return juice;
+const applePieces = cutFruitPieces(apples)
+const orangePieces = cutFruitPieces(oranges)
+const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`
+console.log(juice);
+return juice;
 }
 console.log(fruitProcessor (4, 7))
 
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner= function(avgDolphins, AvgKoalas) {
+if (avgDolphins >= 2 * AvgKoalas) {
+    console.log("Dolphins win!");
+} else if (AvgKoalas >= 2 * avgDolphins) {
+    console.log("Koalas win!")
+} else {
+    console.log("no one wins")
+  }
+} 
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(100, 50);
 
 
