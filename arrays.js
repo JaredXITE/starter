edf74bc62eab66b3d80b6320c6e38dee4ebb47de
Jared Chu <jared@xite.com>
2022-05-19 15:43:01 +0200

    added array example

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 //array example
-
 const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
 const bill = [125, 555, 44];
@@ -11,5 +10,3 @@ const total = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
 console.log("bill", bill);
 console.log("tips", tips);
 console.log("total", total);
-
-
